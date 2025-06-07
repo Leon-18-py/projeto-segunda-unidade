3577be56348c569df34a1ddec81845b978a79c15
Leon-18-py <leonrodobraga.cz@gmail.com>
2025-06-07 18:51:56 -0300

    Update blablacar base.py

--- blablacar base.py
+++ blablacar base.py
@@ -2,6 +2,7 @@ import os
 
 usuarios = list()
 caronas = list()
+reservas = list()
 
 print("*"*50)
 print("Bem vindo!")
@@ -16,7 +17,7 @@ while(True):
     elif(opcao == '1'):
         nome = input("Digite seu nome: ")
         email = input("Digite seu email: ")
-        while(not "@" in email or not email.endswith('.com')):
+        while(not "@" in email or not email.endswith('.com') and "gmail" in email):
             print("Email inválido")
             email = input("Digite seu email: ")
         senha = input("Digite sua senha: ")
@@ -32,7 +33,10 @@ while(True):
                 encontrado = True
                 print("Email já existente. Tente outro: ")
                 email = input("Digite seu email: ")
-                usuario = {"nome": nome,
+                while(not "@" in email or not email.endswith('.com') and "gmail" in email):
+                 print("Email inválido")
+                 email = input("Digite seu email: ")
+            usuario = {"nome": nome,
                  "email": email,
                  "senha": senha
                     }    
@@ -41,17 +45,25 @@ while(True):
         os.system('cls' if os.name == 'nt' else 'clear')
     elif(opcao == '2'):
         email = str(input("Digite o seu email: "))
+        while(not "@" in email or not email.endswith('.com') and "gmail" in email):
+            print("Email inválido")
+            email = input("Digite seu email: ")
         senha = input("Digite a sua senha: ")
         for ind in range(len(usuarios)):
             if(email == usuarios[ind]['email'] and senha == usuarios[ind]['senha']):
                 print("Login efetivado")
                 loggado = -1
                 while(loggado != 0):
-                    print("Menu Login\n")
+                    print("Menu Login")
                     print("Cadastro de carona")
                     print("Listar caronas")
                     print("Buscar carona por origem e destino")
-                    print("Voltar menu inicial\n")
+                    print("Reservar vaga em carona")
+                    print("Cancelar reserva")
+                    print("Remover reserva")
+                    print("Mostrar detalhes de carona")
+                    print("Mostrar caronas cadastradas")
+                    print("Voltar menu inicial")
                     print("Escolha a opcao\n")
                     op=input("Informe a opcao: ")
                     if(op == '1'):
@@ -63,6 +75,7 @@ while(True):
                         horario = str(input("Informe o horário: "))
                         vagas = int(input("Informe a quantidade de vagas: "))
                         valorVaga = float(input("Informe o valor da vaga: "))
+                        passageiro = 0
                         carona = {
                            "nomeMotorista": nomeMotorista,
                            "localOrigem": localOrigem,
@@ -71,6 +84,8 @@ while(True):
                            "horario": horario,
                            "vagas": vagas,
                            "valorVaga": valorVaga,
+                           "email": email,
+                           "passageiro": passageiro
                             }   
                         caronas.append(carona)
                         print("Cadastra feito com sucesso")
@@ -105,6 +120,84 @@ while(True):
                                 print("-"*50)
                         if(not encontrado):
                             print("Carona não existe")
+                    elif(op == '4'):
+                        reserv = False
+                        print("Reservar a sua carona:\n")
+                        emailUsuario = str(input("Informe seu email:"))
+                        while(not "@" in emailUsuario or not emailUsuario.endswith('.com') and "gmail" in emailUsuario):
+                            print("Email inválido")
+                            emailUsuario = input("Digite seu email: ")
+                        dataCarona = str(input("Informe a data da carona: "))
+                        passageiro = 0
+                        for car in caronas:
+                            if(car["data"] == dataCarona):
+                                if(car["vagas"] > 0):
+                                    car["vagas"] -= 1
+                                    passageiro += 1
+                                    reserva = {"emailUsuario": emailUsuario,
+                                               "dataCarona": dataCarona}
+                                    caronas.append(passageiro)
+                                    reservas.append(reserva)
+                                    print("Reserva feito com sucesson\n")
+                                    reserv = True
+                                    break
+                        if (not reserv):
+                            print("Carona não reservada\n")
+                    elif(op == '5'):
+                        emailUsuario = str(input("Informe o email:"))
+                        while(not "@" in emailUsuario or not emailUsuario.endswith('.com') and "gmail" in emailUsuario):
+                            print("Email inválido")
+                            emailUsuario = input("Digite seu email: ")
+                        dataCarona = str(input("Informe a data da carona: "))
+                        indice = -1
+                        for ind in range(len(reservas)):
+                            if(emailUsuario and dataCarona == reservas[ind]["emailUsuario"]["dataCarona"]):
+                                indice = ind
+                            if(indice == -1):
+                                print("Reserva não encontrada")
+                            else:
+                                car["vagas"] += 1
+                                passageiro -= 1
+                                print("Reserva excluída")
+                                reservas.pop(indice)
+                    elif(op == '6'):
+                        dataCarona = str(input("Informe a data da carona: "))
+                        indice = -1
+                        for ind in range(len(caronas)):
+                            if(dataCarona == caronas[ind]["data"]):
+                                indice = ind
+                            if(indice == -1):
+                                print("Carona não encontrada")
+                            else:
+                                print("Carona Removida")
+                                caronas.pop(indice)
+                    elif(op == '7'):
+                        email = str(input("Informe o email: "))
+                        while(not "@" in email or not email.endswith('.com') and "gmail" in email):
+                            print("Email inválido")
+                            email = input("Digite seu email: ")
+                        dataCarona = str(input("Informe a data da carona: "))
+                        for car in caronas:
+                            if(dataCarona == carona["data"] and email == carona["email"]):
+                                print(f"Origem: {car["localOrigem"]}")
+                                print(f"Destino: {car["destino"]}")
+                                print(f"Horário: {car["horario"]}")
+                                print(f"Valor da vaga: {car["valorVaga"]}")
+                                print(f"Vagas restantes: {car["vagas"]}")
+                                print(f"Quantidade de passageiros: {car["passageiro"]}")
+                            else:
+                                print("Carona não encontrada")
+                    elif(op == '8'):
+                      if(email == carona["email"]):
+                        for car in caronas:
+                            print(f"Origem: {car["localOrigem"]}")
+                            print(f"Destino: {car["destino"]}")
+                            print(f"Horario: {car["horario"]}")
+                            print(f"Quantidade de vagas restantes: {car["vagas"]}")
+                            print(f"Valor da vaga: {car["valorVaga"]}")
+                            print(f"Passageiros: {car["passageiro"]}")
+                      else:
+                          print("Nenhuma carona cadastrada")
                     elif(op == '0'):
                         print("Voltando ao menu cadastro")
                         break
